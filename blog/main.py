08fa80eceea2a535e0d3c8fd681e87a399d7b345
safakun <safarovdeem@gmail.com>
2024-03-03 16:14:46 +0500

    refactored ыщьу сщву

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, status, Response, HTTPException
-from . import schemas, models, hashing
+from . import schemas, models
 from fastapi_sqlalchemy import DBSessionMiddleware, db
 
 import os
 from dotenv import load_dotenv
 
 from typing import List
+from .hashing import Hash
 
 
 
@@ -69,11 +70,20 @@ async def show(id, response: Response):
 
 
 
-@app.post('/user')
+@app.post('/user', response_model=schemas.ShowUser)
 async def create_user(request: schemas.User):
     
-    new_user = models.User(name=request.name, email=request.email, password=hashing.Hash.bcrypt(request.password))
+    new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
     db.session.add(new_user)
     db.session.commit()
     db.session.refresh(new_user)
     return new_user
+
+
+@app.get('/user/{id}', response_model=schemas.ShowUser)
+async def get_user(id: int):
+    user = db.session.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f'User with id {id} is not available')
+    
+    return user
--- blog/schemas.py
+++ blog/schemas.py
@@ -21,3 +21,10 @@ class User(BaseModel):
 
     class Config:
         orm_mode = True 
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+
+    class Config:
+        orm_mode = True 
