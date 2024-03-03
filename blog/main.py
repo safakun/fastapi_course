450ad86d47ebd2ba61fc4e0e34b9fb81ed45dada
safakun <safarovdeem@gmail.com>
2024-03-03 11:04:06 +0500

    added statuses

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, status, Response 
 from . import schemas, models
-# from .database import engine
 from fastapi_sqlalchemy import DBSessionMiddleware, db
 
 import os
@@ -18,18 +17,25 @@ app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 # models.Base.metadata.create_all(engine)
 
 
-@app.post('/blog', response_model=schemas.Blog)
+@app.post('/blog', response_model=schemas.Blog, status_code=status.HTTP_201_CREATED)
 async def blog(blog: schemas.Blog):
-    db_book = models.Blog(title=blog.title, body=blog.body)
-    db.session.add(db_book)
+    db_blog = models.Blog(title=blog.title, body=blog.body)
+    db.session.add(db_blog)
     db.session.commit()
-    return db_book
+    return db_blog
 
 @app.get('/blog')
 async def blog():
-    blog = db.session.query(models.Blog).all()
-    return blog
+    blogs = db.session.query(models.Blog).all()
+    return blogs
 
+@app.get('/blog/{id}', status_code=200)
+async def show(id, response: Response):
+    blog = db.session.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        response.status_code = status.HTTP_404_NOT_FOUND 
+        return {'detail': f'Blog with id {id} is not available'}
+    return blog
 
 # @app.post('/blog') 
 # def create(request: schemas.Blog):
