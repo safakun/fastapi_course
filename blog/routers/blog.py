12e1d066eed9a8d7b44c1d369ca1c1916d127512
safakun <safarovdeem@gmail.com>
2024-03-04 15:56:43 +0500

    refactored tags

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -4,9 +4,11 @@ from .. import schemas, models
 from fastapi_sqlalchemy import DBSessionMiddleware, db 
 from fastapi import FastAPI, status, Response, HTTPException
 
-router = APIRouter() 
+router = APIRouter(
+    tags=['blogs']
+) 
 
-@router.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['blogs'])
+@router.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 async def destroy(id):
     blog = db.session.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -17,7 +19,7 @@ async def destroy(id):
    
     return {f'Blog {id} was deleted'}
     
-@router.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED, tags=['blogs'])
+@router.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
 async def update(id, request: schemas.Blog):
     blog = db.session.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -28,19 +30,19 @@ async def update(id, request: schemas.Blog):
     db.session.commit()
     return 'updated'
 
-@router.get('/blog', response_model=List[schemas.ShowBlog], tags=['blogs'])
+@router.get('/blog', response_model=List[schemas.ShowBlog])
 async def blog():
     blogs = db.session.query(models.Blog).all()
     return blogs 
 
-@router.post('/blog', response_model=schemas.Blog, status_code=status.HTTP_201_CREATED, tags=['blogs'])
+@router.post('/blog', response_model=schemas.Blog, status_code=status.HTTP_201_CREATED)
 async def blog(blog: schemas.Blog):
     db_blog = models.Blog(title=blog.title, body=blog.body)
     db.session.add(db_blog)
     db.session.commit()
     return db_blog 
 
-@router.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog, tags=['blogs'])
+@router.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 async def show(id, response: Response):
     blog = db.session.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
--- blog/routers/user.py
+++ blog/routers/user.py
@@ -5,10 +5,12 @@ from fastapi_sqlalchemy import DBSessionMiddleware, db
 from fastapi import FastAPI, status, Response, HTTPException 
 from ..hashing import Hash
 
-router = APIRouter() 
+router = APIRouter(
+    tags=['users']
+) 
 
 
-@router.post('/user', response_model=schemas.ShowUser, tags=['users'])
+@router.post('/user', response_model=schemas.ShowUser)
 async def create_user(request: schemas.User):
     
     new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
@@ -18,7 +20,7 @@ async def create_user(request: schemas.User):
     return new_user
 
 
-@router.get('/user/{id}', response_model=schemas.ShowUser, tags=['users'])
+@router.get('/user/{id}', response_model=schemas.ShowUser)
 async def get_user(id: int):
     user = db.session.query(models.User).filter(models.User.id == id).first()
     if not user:
